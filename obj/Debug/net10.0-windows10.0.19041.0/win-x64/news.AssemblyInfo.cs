d08a904c59315cf5bbac8c1fa238ba994e536f6b
Tito Mutethia <titomutethia@gmail.com>
2026-08-17 16:14:01 +0300

    Added currentDateTime property that updates every time the forecast is loaded

--- obj/Debug/net10.0-windows10.0.19041.0/win-x64/news.AssemblyInfo.cs
+++ obj/Debug/net10.0-windows10.0.19041.0/win-x64/news.AssemblyInfo.cs
@@ -17,7 +17,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("news")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0+30b397250a343066f20dff5cec4d088c8e16139a")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0+528684d37e8c40f6c63399ed55225a41e46cd9e5")]
 [assembly: System.Reflection.AssemblyProductAttribute("news")]
 [assembly: System.Reflection.AssemblyTitleAttribute("news")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
